26cc3aeb138118def79d853b7403ed8e33122a61
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-06-21 01:05:04 +0530

    JS

--- script.js
+++ script.js
@@ -8,4 +8,28 @@ function clearDisplay() {
     display.value = "";
 }
 
-function
+
+function calculate () {
+    let input = display.value.trim();
+    let operators = ['+', '-', 'x', '÷'];
+    let numbers = [];
+
+    let operatorsFound = false
+    // Split the input if it contains any operators
+
+
+    for (let i=0; i<operators.length; i++)
+        {
+            if (input.includes(operators[i]))
+                {
+                    operatorsFound = true;
+                    numbers = input.split(operators[i]);
+                    break;
+                }
+        }
+    console.log(numbers);
+    
+    if (!operatorsFound){
+        display.value = "Errorrr";
+    }
+}
